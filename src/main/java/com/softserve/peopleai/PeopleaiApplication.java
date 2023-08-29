c1813d626abbf6e2f9365d9779f903fdc625de77
Marharyta Kozub <MARHARYTAKO@ext.inditex.com>
2023-08-29 11:22:59 +0300

    UC#1 Infrastructure setup

---
+++ src/main/java/com/softserve/peopleai/PeopleaiApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.peopleai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softserve/peopleai/PeopleaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.peopleai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
