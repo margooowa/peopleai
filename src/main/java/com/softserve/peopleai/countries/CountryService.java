ab664c3c8cb85b127c9cf135bf1d92394b5b1100
Marharyta Kozub <MARHARYTAKO@ext.inditex.com>
2023-08-29 12:18:12 +0300

    UC#1 Create filter function by name

--- src/main/java/com/softserve/peopleai/countries/CountryService.java
+++ src/main/java/com/softserve/peopleai/countries/CountryService.java
@@ -1,13 +1,13 @@
 package com.softserve.peopleai.countries;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CountryService {
-
 
   private final CountryClient countryClient;
 
@@ -16,9 +16,16 @@ public class CountryService {
     this.countryClient = countryClient;
   }
 
-
   public List<Country> retrieveCounties() {
     List<Country> countriesAll = countryClient.getCountriesFromApi();
     return countriesAll;
+  }
+
+  public List<Country> filterCountriesByName(List<Country> countries, String searchString) {
+    String searchLowerCase = searchString.toLowerCase();
+
+    return countries.stream()
+        .filter(country -> country.getName().getCommon().toLowerCase().contains(searchLowerCase))
+        .collect(Collectors.toList());
   }
 }
